e62edf8ad4625ff4c370c2dc5963cd0c7a911e25
Gurparteek <gdhream@gmail.com>
2015-07-07 12:54:47 -0400

    code for the board

---
+++ board.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python
+
+board =[]
+for i in range (9):
+	board.append(-1)
+print board
+
+def print_board(board):
+
+
+	for i in range(3):
+		print " ",
+		for j in range(3):
+			if board[i*3+j] == 1:
+				print 'X',
+			elif board[i*3+j] == 0:
+				print 'O',
+			else:
+				print ' ',
+
+			if j != 2:
+				print " | ",
+		print
+
+		if i != 2:
+			print "-----------------"
+		else:
+			print 
