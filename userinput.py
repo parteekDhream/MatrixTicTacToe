6166e3f22cba5337cba4e011424763358c10d71d
Abdullah S. Abbas <abdullahsaud@live.ca>
2015-07-08 17:24:05 -0400

    I added the user input file

---
+++ userinput.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+def functionA(p1,p2,rule):
+  return p1
+  return p2
+  return rule
+
+
+# Main 
+
+print "1- P1 Vs Computer"
+print "2- P1 Vs P2"
+print "3- Rules"
+
+userniput=input('Select please')
+
+if userniput ==1:
+  print "Hey"
+elif userniput==2:
+  print "sjkfd"
+elif userniput==3:
+  print "hi"
+else:
+  print "You did not choose the correct input"
